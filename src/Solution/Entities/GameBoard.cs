78e88faf61456ab97d3040cc62bcabb15203f933
Oleh Prostakov <110679395+Its-OP@users.noreply.github.com>
2024-12-28 15:40:23 +0200

    Implement GameBoard

---
+++ src/Solution/Entities/GameBoard.cs
@@ -0,0 +1,47 @@
+﻿using Solution.Utils;
+
+namespace Solution.Entities;
+
+public class GameBoard
+{
+    const int EmptyTilePlaceholder = 0;
+    private readonly int[,] _board;
+
+    public GameBoard(int[,] tiles)
+    {
+        Guard.That(tiles.GetLength(0) == tiles.GetLength(1), "Tiles must be positioned in a square");
+        var (tilesAreValid, validationError) = SetOfTilesIsValid(tiles);
+        Guard.That(tilesAreValid, validationError);
+        
+        _board = tiles;
+    }
+
+    public int[,] GetTiles()
+    {
+        return (int[,])_board.Clone();
+    }
+
+    private (bool, string) SetOfTilesIsValid(int[,] tiles)
+    {
+        var range = tiles.GetLength(0) * tiles.GetLength(1);
+        var set = new HashSet<int>(range);
+        var maxElement = range - 1;
+        var minElement = EmptyTilePlaceholder;
+        
+        for (var i = 0; i < tiles.GetLength(0); i++) 
+        { 
+            for (var j = 0; j < tiles.GetLength(1); j++) 
+            { 
+                var tile = tiles[i, j]; 
+                if (tile < minElement || tile > maxElement)
+                    return (false, $"Tile {tile} is out of range");
+                if (set.Contains(tile))
+                    return (false, $"Tile {tile} is duplicated");
+                
+                set.Add(tile);
+            } 
+        }
+        
+        return (true, string.Empty);
+    }
+}
---
+++ src/UnitTests/BoardTests/GameBoardInitializationTests.cs
@@ -0,0 +1,39 @@
+﻿using Solution.Entities;
+
+namespace UnitTests.BoardTests;
+
+public class BoardInitializationTests
+{
+    [Test]
+    public void InitializeBoard_TilesArePositionedCorrectly_UseTilesAsInitialBoard()
+    {
+        // Arrange
+        int[,] tiles = { { 0, 1, 2, 3 }, { 4, 5, 6, 7 }, { 8, 9, 10, 11 }, { 12, 13, 14, 15 } };
+        
+        // Act
+        var board = new GameBoard(tiles);
+        
+        // Assert
+        Assert.That(board.GetTiles(), Is.EqualTo(tiles));
+    }
+    
+    [Test]
+    public void InitializeBoard_TilesAreNotPositionedAsSquare_ThrowException()
+    {
+        // Arrange
+        int[,] nonSquaredTiles = { { 0, 1, 2, 3 }, { 4, 5, 6, 7 }, { 8, 9, 10, 11 } };
+        
+        // Act + Assert
+        Assert.That(() => new GameBoard(nonSquaredTiles), Throws.Exception);
+    }
+    
+    [Test]
+    public void InitializeBoard_TilesContainInvalidNumber_ThrowException()
+    {
+        // Arrange
+        int[,] tilesWithInvalidNumber = { { 0, 1, 2, 3 }, { 4, 5, 6, 7 }, { 8, 9, 10, 11 }, { 12, 13, 14, 100 } };
+        
+        // Act + Assert
+        Assert.That(() => new GameBoard(tilesWithInvalidNumber), Throws.Exception);
+    }
+}
