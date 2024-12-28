c76d8cbe1f683cb65900cb8d7411624ffb340245
Oleh Prostakov <110679395+Its-OP@users.noreply.github.com>
2024-12-28 15:00:59 +0200

    Add Guards

---
+++ src/Solution/Utils/Guard.cs
@@ -0,0 +1,10 @@
+﻿namespace Solution.Utils;
+
+public static class Guard
+{
+    public static void That(bool condition, string message)
+    {
+        if (!condition)
+            throw new InvalidOperationException(message);
+    }
+}
---
+++ src/UnitTests/GuardUnitTests.cs
@@ -0,0 +1,26 @@
+﻿using Solution.Utils;
+
+namespace UnitTests;
+
+public class GuardUnitTests
+{
+    [Test]
+    public void GuardThat_ConditionIsTrue_DoNotThrow()
+    {
+        // Arrange
+        const bool condition = true;
+        
+        // Act + Assert
+        Assert.That(() => Guard.That(condition, string.Empty), Throws.Nothing);
+    }
+    
+    [Test]
+    public void GuardThat_ConditionIsTrue_ThrowInvalidOperationException()
+    {
+        // Arrange
+        const bool condition = false;
+        
+        // Act + Assert
+        Assert.That(() => Guard.That(condition, string.Empty), Throws.InvalidOperationException);
+    }
+}
