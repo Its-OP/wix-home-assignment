051bb4f86fbfe370364c0e56f48971000bc5fd98
Oleh Prostakov <110679395+Its-OP@users.noreply.github.com>
2024-12-28 21:09:13 +0200

    Move ToString to a dedicated GameBoardStringifier

--- src/Solution/Entities/GameBoard.cs
+++ src/Solution/Entities/GameBoard.cs
@@ -134,35 +134,4 @@ public class GameBoard
         
         return deviation;
     }
-
-    public override string ToString()
-    {
-        var rows = _tilesConfiguration.GetLength(0);
-        var cols = _tilesConfiguration.GetLength(1);
-        
-        // Find length of the largest number, so all the columns can be padded to the same width
-        var columnWidth = _tilesConfiguration.Cast<int>().Max(num => num).ToString().Length;
-        var result = new StringBuilder();
-
-        for (var i = 0; i < rows; i++)
-        {
-            for (var j = 0; j < cols; j++)
-            {
-                // Format each number with padding
-                var formattedNumber = _tilesConfiguration[i, j].ToString().PadLeft(columnWidth);
-                result.Append(formattedNumber);
-                
-                if (j < cols - 1)
-                {
-                    result.Append(' ');
-                }
-            }
-            if (i < rows - 1)
-            {
-                result.AppendLine();
-            }
-        }
-
-        return result.ToString();
-    }
 }
---
+++ src/UnitTests/GameBoardStringifierUnitTests.cs
@@ -0,0 +1,25 @@
+﻿using Solution.Presentation;
+
+namespace UnitTests;
+
+public class GameBoardStringifierUnitTests
+{
+    [Test]
+    public void ToString_ReturnCorrectStringRepresentation()
+    {
+        // Arrange
+        var tilesConfiguration = new [,] { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 }, { 13, 14, 15, 0 } };
+        const string expectedStringifiedTilesConfiguration = """
+                                                 1  2  3  4
+                                                 5  6  7  8
+                                                 9 10 11 12
+                                                13 14 15  0
+                                                """;
+        
+        // Act
+        var stringifiedTilesConfiguration = GameBoardStringifier.ToString(tilesConfiguration);
+        
+        // Assert
+        Assert.That(stringifiedTilesConfiguration, Is.EqualTo(expectedStringifiedTilesConfiguration));
+    }
+}
