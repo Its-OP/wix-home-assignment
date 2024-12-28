8a58800678f9547f13ba371b25306bba2f14903f
Oleh Prostakov <110679395+Its-OP@users.noreply.github.com>
2024-12-28 21:53:44 +0200

    Move presentation to a separate file

---
+++ src/Solution/Presentation/ConsolePresentor.cs
@@ -0,0 +1,51 @@
+﻿using Solution.Entities;
+using Solution.Utils;
+
+namespace Solution.Presentation;
+
+public static class ConsolePresentation
+{
+    public static void PlayGame(int boardSize)
+    {
+        var gameBoard = new GameBoard(TilesConfigurationGenerator.Generate(boardSize));
+        Console.WriteLine("Welcome to 15 Puzzle! Press any key to continue...");
+        Console.ReadKey();
+
+        while (!gameBoard.IsInWinningConfiguration)
+        {
+            PrintGameBoard(gameBoard);
+            Console.WriteLine("Type tile that you would like to move");
+            var tileString = Console.ReadLine();
+            if (!int.TryParse(tileString, out var tile))
+            {
+                Console.Error.WriteLine("Tile must be a natural number or 0!");
+                continue;
+            }
+    
+            if (!gameBoard.HasTile(tile))
+            {
+                Console.Error.WriteLine($"Tile {tile} is not on the game board!");
+                continue;
+            }
+    
+            if (!gameBoard.MoveTile(tile))
+            {
+                Console.Error.WriteLine($"Tile {tile} cannot be moved!");
+                continue;
+            }
+        }
+
+        Console.WriteLine("Congratulations! You've solved the puzzle! Press any key to exit.");
+        Console.ReadKey();
+        return;
+
+        static void PrintGameBoard(GameBoard gameBoard)
+        {
+            var rowLength = GameBoardStringifier.GetStringRowLength(gameBoard);
+            var lineDivider = new string('=', rowLength);
+            Console.WriteLine(lineDivider);
+            Console.WriteLine(GameBoardStringifier.ToString(gameBoard));
+            Console.WriteLine(lineDivider);
+        }
+    }
+}
--- src/Solution/Program.cs
+++ src/Solution/Program.cs
@@ -1,46 +1,4 @@
-﻿using Solution.Entities;
-using Solution.Presentation;
-using Solution.Utils;
+﻿using Solution.Presentation;
 
 const int defaultBoardSize = 4;
-
-var gameBoard = new GameBoard(TilesConfigurationGenerator.Generate(defaultBoardSize));
-Console.WriteLine("Welcome to 15 Puzzle! Press any key to continue...");
-Console.ReadKey();
-
-while (!gameBoard.IsInWinningConfiguration)
-{
-    PrintGameBoard(gameBoard);
-    Console.WriteLine("Type tile that you would like to move");
-    var tileString = Console.ReadLine();
-    if (!int.TryParse(tileString, out var tile))
-    {
-        Console.Error.WriteLine("Tile must be a natural number or 0!");
-        continue;
-    }
-    
-    if (!gameBoard.HasTile(tile))
-    {
-        Console.Error.WriteLine($"Tile {tile} is not on the game board!");
-        continue;
-    }
-    
-    if (!gameBoard.MoveTile(tile))
-    {
-        Console.Error.WriteLine($"Tile {tile} cannot be moved!");
-        continue;
-    }
-}
-
-Console.WriteLine("Congratulations! You've solved the puzzle! Press any key to exit.");
-Console.ReadKey();
-return;
-
-static void PrintGameBoard(GameBoard gameBoard)
-{
-    var rowLength = GameBoardStringifier.GetStringRowLength(gameBoard);
-    var lineDivider = new string('=', rowLength);
-    Console.WriteLine(lineDivider);
-    Console.WriteLine(GameBoardStringifier.ToString(gameBoard));
-    Console.WriteLine(lineDivider);
-}
+ConsolePresentation.PlayGame(defaultBoardSize);
