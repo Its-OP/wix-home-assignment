5421b6a76e51f784c75a4f9800b7a3162ff266e4
Oleh Prostakov <110679395+Its-OP@users.noreply.github.com>
2025-07-05 02:12:00 +0300

    Add board generator

---
+++ SnakesAndLadders/BoardGenerator.cs
@@ -0,0 +1,67 @@
+﻿namespace SnakesAndLadders;
+
+public class BoardGenerator
+{
+    // If we get 6 or more snakes in a row, the board becomes unsolvable
+    private const int MaxNumberOfConcurrentSnakes = 5;
+    
+    // How likely a snake or a ladder is to spawn on a tile
+    private readonly float _snakeOrLadderGenerationProbability;
+    
+    // When either a snake or a ladder has spawned, how likely it is to be a ladder
+    private readonly float _ladderProbability;
+    
+    public BoardGenerator(float snakeOrLadderGenerationProbability = 0.1f, float ladderProbability = 0.5f)
+    {
+        if (snakeOrLadderGenerationProbability is < 0 or > 1)
+            throw new ArgumentOutOfRangeException($"Snake or Ladder probability of {snakeOrLadderGenerationProbability} is out of the [0, 1] range");
+        
+        if (ladderProbability is < 0 or > 1)
+            throw new ArgumentOutOfRangeException($"Ladder probability of {ladderProbability} is out of the [0, 1] range");
+        
+        _snakeOrLadderGenerationProbability = snakeOrLadderGenerationProbability;
+        _ladderProbability = ladderProbability;
+    }
+
+    public IReadOnlyList<int> GenerateBoard(int numberOfTiles)
+    {
+        if (numberOfTiles <= 1)
+            throw new ArgumentException("Board must have at least one tile");
+        
+        var board = new int[numberOfTiles];
+        var random = new Random();
+        var numberOfConcurrentSnakes = 0;
+        
+        // First and last tiles must not have either snakes or ladders
+        for (var i = 1; i < numberOfTiles - 1; i++)
+        {
+            var currentTileValue = i;
+            var snakeOrLadderHasSpawned = random.NextSingle() < _snakeOrLadderGenerationProbability;
+            if (snakeOrLadderHasSpawned)
+            {
+                var ladderHasSpawned = random.NextSingle() < _ladderProbability;
+                if (ladderHasSpawned || numberOfConcurrentSnakes >= MaxNumberOfConcurrentSnakes)
+                {
+                    // Ladder goes to at least next tile
+                    currentTileValue = random.Next(i + 1, numberOfTiles - 1);
+                    numberOfConcurrentSnakes = 0;
+                }
+                else
+                {
+                    // Snake goes to at most previous tile
+                    currentTileValue = random.Next(0, i - 1);
+                    numberOfConcurrentSnakes += 1;
+                }
+            }
+            else
+            {
+                numberOfConcurrentSnakes = 0;
+            }
+            
+            board[i] = currentTileValue;
+        }
+
+        board[^1] = numberOfTiles - 1;
+        return board;
+    }
+}
---
+++ UnitTests/BoardGeneratorUnitTests.cs
@@ -0,0 +1,96 @@
+﻿using NUnit.Framework;
+using SnakesAndLadders;
+
+namespace UnitTests;
+
+public class BoardGeneratorUnitTests
+{
+    private const int NumberOfTiles = 100;
+    
+    [Test]
+    public void GenerateBoard_ZeroProbabilityForSnakeOrLadder_EveryTilePointsToItself()
+    {
+        // Arrange
+        var generator = new BoardGenerator(0, 0);
+        
+        // Act
+        var board = generator.GenerateBoard(NumberOfTiles);
+        var expectedBoardIndices = Enumerable.Range(0, NumberOfTiles).ToList();
+        
+        // Assert
+        Assert.That(board, Is.EqualTo(expectedBoardIndices));
+    }
+    
+    [Test]
+    public void GenerateBoard_MaxProbabilityForSnakeOrLadder_FirstAndLastTilesAreClearOfSnakesOrLadders()
+    {
+        // Arrange
+        var generator = new BoardGenerator(snakeOrLadderGenerationProbability: 1);
+        
+        // Act
+        var board = generator.GenerateBoard(NumberOfTiles);
+        var expectedBoardIndices = Enumerable.Range(0, NumberOfTiles).ToList();
+        
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(board[0], Is.EqualTo(0));
+            Assert.That(board[^1], Is.EqualTo(NumberOfTiles - 1));
+        });
+    }
+
+    [Test]
+    public void GenerateBoard_MaxProbabilityForSnakes_BoardRemainsSolvable()
+    {
+        // Arrange
+        var generator = new BoardGenerator(1, 0);
+        
+        // Act
+        var board = generator.GenerateBoard(NumberOfTiles);
+        var solution = Solver.SolveBoard(board);
+        
+        // Assert
+        Assert.That(solution, Is.Not.Empty);
+    }
+    
+    [Test]
+    // A ladder is spawned after every 5 snakes
+    public void GenerateBoard_MaxProbabilityForSnakes_SpawnSixteenLadders()
+    {
+        // Arrange
+        var generator = new BoardGenerator(1, 0);
+        
+        // Act
+        var board = generator.GenerateBoard(NumberOfTiles);
+        var mapOfSnakes = new List<bool>();
+        for (var i = 0; i < NumberOfTiles; i++)
+        {
+            // Add False if ladder was spawned, True otherwise
+            mapOfSnakes.Add(board[i] <= i);
+        }
+        var numLadders = mapOfSnakes.Count(x => x == false);
+        
+        // Assert
+        Assert.That(numLadders, Is.EqualTo(16));
+    }
+    
+    [Test]
+    public void GenerateBoard_MaxProbabilityForLadders_SpawnOnlyLadders()
+    {
+        // Arrange
+        var generator = new BoardGenerator(1, 1);
+        
+        // Act
+        var board = generator.GenerateBoard(NumberOfTiles);
+        var mapOfLadders = new List<bool>();
+        // Skip first and last tiles
+        for (var i = 1; i < NumberOfTiles-1; i++)
+        {
+            // Add False if ladder was spawned, True otherwise
+            mapOfLadders.Add(board[i] >= i);
+        }
+        
+        // Assert
+        Assert.That(mapOfLadders, Is.All.True);
+    }
+}
